89c8b8158779954facc3f59c33d4f6c0f45d61c6
namtrung205 <36217880+namtrung205@users.noreply.github.com>
2018-10-24 22:35:12 +0700

    Update 24/10/2018

---
+++ WORKING WITH BLOCK - 23-10/WORKING WITH BLOCK/Cmd Layer.cs
@@ -0,0 +1,57 @@
+﻿
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Colors;
+
+//
+using commonFunctions;
+
+
+namespace myCustomCmds
+{
+    public class CmdLayer
+    {
+        [CommandMethod("NL")]
+        public static void createALayerByName(string layerName)
+        {
+            // Get the current document and database
+            Document acDoc = Application.DocumentManager.MdiActiveDocument;
+            Database acCurDb = acDoc.Database;
+
+            // Start a transaction
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Layer table for read
+                LayerTable acLyrTbl;
+                acLyrTbl = acTrans.GetObject(acCurDb.LayerTableId,
+                                                OpenMode.ForRead) as LayerTable;
+
+                string sLayerName = layerName;
+
+                if (acLyrTbl.Has(sLayerName) == false)
+                {
+                    using (LayerTableRecord acLyrTblRec = new LayerTableRecord())
+                    {
+                        acLyrTblRec.Name = sLayerName;
+
+                        // Upgrade the Layer table for write
+                        acLyrTbl.UpgradeOpen();
+
+                        // Append the new layer to the Layer table and the transaction
+                        acLyrTbl.Add(acLyrTblRec);
+                        acTrans.AddNewlyCreatedDBObject(acLyrTblRec, true);
+                    }
+                }
+
+                // Save the changes and dispose of the transaction
+                acTrans.Commit();
+            }
+        }
+
+    }
+}
+
+
+
